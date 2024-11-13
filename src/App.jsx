65a24cd01c8bf0179032ccf175c8397206345835
Harutune Davis <d.harutune@wustl.edu>
2024-11-13 15:47:03 -0600

    Added function square

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function Square() {
+  return <button className="square">1</button>;
+}
 export default function Board() {
   return (
     <>
