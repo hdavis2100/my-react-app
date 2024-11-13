dba821300820b8beb210fa53d90d1c9b70198b24
Harutune Davis <d.harutune@wustl.edu>
2024-11-13 16:30:09 -0600

    Added calculate winner function to  board function to check if there is winner and print the winner. I now have a working tictactoe game.

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,13 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   return (
     <>
